626d3e2190be068767604b57aead30510ccdec67
stella47 <stella47@users.noreply.github.com>
2016-11-07 09:08:17 -0800

    Update marg_fleet_mvp.py

--- marg_fleet_mvp.py
+++ marg_fleet_mvp.py
@@ -18,8 +18,9 @@ from gurobipy import *
 
 
 
-
-
+#data for charging station, Tim or Varsh to fill out
+max_charge_rate = 0 
+min_charge_rate = 0
 
 
 
@@ -36,7 +37,7 @@ csvFile = csv.reader(file)
 # Pull out headers
 Header = csvFile.next()
 
-# Make a multidict with all patient information
+# Make a multidict with all route information
 route_name, route_distance, route_hours, service_hours, loops_day, active_evs = multidict({row[0]: row[1:len(Header)] for row in csvFile})
 
 # Fix data type for Age, Waiting Time, and Life Expectancy, since they are all currently stored as strings (characters)
@@ -51,23 +52,40 @@ for r in route_name:
 
 # import price data 
 
-file = open("files/prices.csv", "rU")
-csvFile = csv.reader(file)
+print route_distance
 
-# Pull out headers
-Header = csvFile.next()
+# build objective function
 
-# Make a multidict with all patient information
-hour_price,	summer_weekday,	summer_weekend,	winter_weekday,	winter_weekend = multidict({row[0]: row[1:len(Header)] for row in csvFile})
 
-# Fix data type for Age, Waiting Time, and Life Expectancy, since they are all currently stored as strings (characters)
-for h in hour_price:
-	summer_weekday[h] = float(summer_weekday[h])
-	summer_weekend[h] = float(summer_weekend[h])
-	winter_weekday[h] = float(winter_weekday[h])
-	winter_weekend[h] = float(winter_weekend[h])
 
-print summer_weekend
+
+# build decision variables
+### route_assignment: for each bus, for each hour, choose route assignment (0,1,2,3,4,5)
+
+### charge_or_not: for each bus, for each hour, binary decision variable to determine charging (1), or not charging (0)
+
+### charge_rate: for each bus, for each hour, continuous decision variable of rate of charging, bounded by highest charge rate
+
+
+
+# build constraints
+## constraint 1: # of buses allowed on the same route at the same time
+
+## constraint 2: # of hours it takes to complete each route
+
+## State-of-charge constraint, cannot over charge the batteries to exceed the maximum capacity of the battery
+
+## Cannot be charging if the bus in on a route
+   ##if route_assignment is not 0, charge_or_not = 0 
+   ##if route_assignment is 0, charge_or_not can be 0 or 1
+   ##if charge_or_not = 0, charge_rate = 0
+   ##if charge_or_not = 1, 0<charge_rate<max_charge_rate
+
+
+
+
+
+
 
 
 
